3065387b36d209ceae055ced35e22eba6222c9d6
Victoria Rosa <vickrosa7@hotmail.com>
2022-01-28 11:24:38 -0300

    28

--- frontend/home/script.js
+++ frontend/home/script.js
@@ -17,11 +17,11 @@ var seuAlerta;
           seuAlerta = coord
 
           alerta.hidden = false;
+          
           cadastrar.hidden = false;
       });
 
       navigator.geolocation.getCurrentPosition((location) => {
-        
         let coord = { lat: location.coords.latitude, lng: location.coords.longitude };
 
         map.setCenter(coord);
@@ -31,11 +31,37 @@ var seuAlerta;
  }
 
  function cadastro() {
-     let tipo = alerta.value;
-     addMarker(seuAlerta, tipo, "../assets/radar.png");
-     alerta.hidden = true;
-     cadastrar.hidden = true;
+  let data = JSON.stringify({
+      "id_user": 1,
+      "id_alerta": alerta.value,
+      "coordenadas": seuAlerta.lat + "," + seuAlerta.lng,
+      "ativo": true,
+  });
 
+   fetch("http://localhost:3000/localizacao", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: data
+
+   })
+   .then(resp => { return resp.json() })
+   .then(data => {
+      if(data.id != undefined) {
+        let tipo = alerta.value;
+        addMarker(seuAlerta, tipo, "../assets/destination.png");
+        alerta.hidden = true;
+        cadastrar.hidden = true;
+      } else {
+        alert("Não foi possível informar o alerta");
+      }
+   })
+ }
+
+ function inicializar() {
+   carregarMarcacoes();
+   carregarAlertas();
  }
 
  function addMarker(coord, title, image) {
@@ -45,4 +71,31 @@ var seuAlerta;
         title: title,
         icon: image
       });
+ }
+
+ function carregarMarcacoes() {
+   fetch("http://localhost:3000/localizacao")
+   .then( resp => { return resp.json()})
+   .then(data => {
+     data.forEach(localizacao => {
+        //localizacao.coordenadas => '-22.702155383462557,-46.99188606123809'
+        let coordenadas = localizacao.coordenadas.split(',');
+        let coord = { lat: Number(coordenadas[0]), lng: Number(coordenadas[1]) };
+
+        addMarker(coord, localizacao.alertum.tipo, "../assets/destination.png");
+     });
+   })
+ }
+
+ function carregarAlertas() {
+   fetch("http://localhost:3000/alerta")
+   .then(resp => { return resp.json()})
+   .then(data => {
+     data.forEach(alert => {
+       let op = document.createElement("option");
+       op.value = alert.id;
+       op.innerHTML = alert.tipo;
+       alerta.appendChild(op);
+     })
+   })
  }
