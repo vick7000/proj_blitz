d927531156937245eaaa02be7b5b2d2b8a2746a9
Victoria Rosa <vickrosa7@hotmail.com>
2022-02-09 11:24:08 -0300

    9/2

--- mobile/pages/home/home.js
+++ mobile/pages/home/home.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Dimensions, StyleSheet, ToastAndroid } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, Text, Dimensions, StyleSheet, ToastAndroid, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 export default function Home() {
+    const [marcadores, setMarcadores] = useState([]);
     const [coord, setCoord] = useState({
         latitude: 37.78825,
         longitude: -122.4324
@@ -15,9 +16,26 @@ export default function Home() {
         if (status !== 'granted') {
             ToastAndroid.show('Localização negada', ToastAndroid.SHORT);
         } else {
-            let location = await Location.getCurrentPositionAsync({});
+            let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.BestForNavigation});
 
             console.log(location);
+            setCoord({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude
+            });
+
+            let posi = {
+                coordenadas: location.coords.latitude + ',' + location.coords.longitude,
+                alertum: {
+                    tipo: "marker",
+                    descricao: "Minha Localização"
+                },
+            }
+            let arr = [];
+
+            arr.push(posi);
+
+            setMarcadores(arr);
         }
 
     }, []);
@@ -28,11 +46,29 @@ export default function Home() {
             region={
                 region= {
                     ...coord,
-                    latitudeDelta: 0.05,
-                    longitudeDelta: 0.05
-                  }
-        
-            }/>
+                    latitudeDelta: 0.0065,
+                    longitudeDelta: 0.00065
+            }}>
+                {
+                marcadores.map((marcador, index) => {
+                    let loki = marcador.coordenadas.split(',');
+                    return (
+                        <Marker
+                            key={index}
+                            coordinate={{
+                                latitude: Number(loki[0]),
+                                longitude: Number(loki [1])              
+                            }}
+                            title={marcador.alertum.tipo}
+                            description={"Texto qualquer  2"}
+                        >
+                            <Image source={require('../../assets/app/'+ marcador.alertum.tipo + '.png')} style={styles.marcador}/>
+                        </Marker>
+                        )
+                    })
+                }
+                
+            </MapView>
         </View>
     )
 }
@@ -47,5 +83,9 @@ const styles = StyleSheet.create({
     map: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height
+    },
+    marcador: {
+        width: 32,
+        height: 32
     }
 });
